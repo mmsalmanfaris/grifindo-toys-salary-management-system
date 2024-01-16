cbcb0ccfff7642619a2c36c497e5afb143dfedb6
mmsalmanfaris <mmsalmanfaaris01@gmail.com>
2024-01-16 23:25:15 +0530

    Report Genarator Configuration

---
+++ Grifindo Toys/EmpReport.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Grifindo_Toys
+{
+    partial class EmpReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.btn_new = new System.Windows.Forms.Button();
+            this.cmb_empid = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(12, 137);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(1285, 601);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // btn_new
+            // 
+            this.btn_new.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(57)))), ((int)(((byte)(70)))));
+            this.btn_new.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_new.Font = new System.Drawing.Font("Poppins SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_new.ForeColor = System.Drawing.Color.White;
+            this.btn_new.Location = new System.Drawing.Point(455, 44);
+            this.btn_new.Name = "btn_new";
+            this.btn_new.Size = new System.Drawing.Size(156, 45);
+            this.btn_new.TabIndex = 70;
+            this.btn_new.Text = "New";
+            this.btn_new.UseVisualStyleBackColor = false;
+            // 
+            // cmb_empid
+            // 
+            this.cmb_empid.Font = new System.Drawing.Font("Poppins", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_empid.FormattingEnabled = true;
+            this.cmb_empid.Location = new System.Drawing.Point(210, 49);
+            this.cmb_empid.Name = "cmb_empid";
+            this.cmb_empid.Size = new System.Drawing.Size(218, 38);
+            this.cmb_empid.TabIndex = 69;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 31);
+            this.label3.TabIndex = 68;
+            this.label3.Text = "Select Employee:";
+            this.label3.UseWaitCursor = true;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // EmpReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1309, 750);
+            this.Controls.Add(this.btn_new);
+            this.Controls.Add(this.cmb_empid);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "EmpReport";
+            this.Text = "EmpReport";
+            this.Load += new System.EventHandler(this.EmpReport_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.Button btn_new;
+        private System.Windows.Forms.ComboBox cmb_empid;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Grifindo Toys/EmpReport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grifindo_Toys
+{
+    public partial class EmpReport : Form
+    {
+        public EmpReport()
+        {
+            InitializeComponent();
+        }
+
+        private void EmpReport_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
